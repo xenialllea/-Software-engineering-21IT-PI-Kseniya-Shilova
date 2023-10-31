ef9bd2f21a684d84e670ad07edab0328515603fc
xenialllea <125434826+xenialllea@users.noreply.github.com>
2023-10-31 22:56:49 +0300

    В конструкторах добавлена проверка на null для списков

--- Laboratory work 3/Laboratory work 3/Order.cs
+++ Laboratory work 3/Laboratory work 3/Order.cs
@@ -3,12 +3,20 @@ namespace Laboratory_work_3;
 public class Order
 {
     public string OrderNumber { get; set; }
-    public List<Photo> PhotoList { get; set; }
+    public List<Photo>? PhotoList { get; set; }
 
-    public Order(string orderNumber, List<Photo> photoList)
+    public Order(string orderNumber, List<Photo>? photoList)
     {
-        PhotoList = photoList;
-    
+        if (photoList == null || photoList.Count == 0)
+        {
+            throw new ArgumentException(null, nameof(photoList));
+        }
+        else
+        {
+            PhotoList = photoList;
+        }
+
+
         if (string.IsNullOrEmpty(orderNumber))
         {
             throw new ArgumentException(null, nameof(orderNumber));
